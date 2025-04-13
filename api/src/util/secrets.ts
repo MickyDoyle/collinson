6bd8b5deabff05234ac85cd62e21ed9e0a1431e7
Micky Doyle <micky.doyle@essensys.tech>
2025-04-13 20:08:16 +0100

    remove key

--- api/src/util/secrets.ts
+++ api/src/util/secrets.ts
@@ -1,6 +1,1 @@
-export const amadeus = {
-  apiKey: 'w1GBHElWfCnSX2ACESJrub6fmqwvxKUV',
-  apiSecret: 'QVnrpNR6AxfBAx9F',
-};
-
 export const googleSecret = { apiKey: 'AIzaSyCVpaTO9cwkJ0AhbZzPSjO7tuP83TmmiJE' };
